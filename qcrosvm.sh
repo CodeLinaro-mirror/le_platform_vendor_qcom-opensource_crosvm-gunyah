7f23c882ef5bcba40a924a65a4f54dad99345729
Jianguo Sun <jianguos@qti.qualcomm.com>
2025-09-17 14:12:05 +0200

    qcrosvm.sh: update the comments and the bias count
    Update the comments and the bias count.
    
    Change-Id: Ice0257bf2a642daf83d90d410a9797b6aa54511b

--- qcrosvm.sh
+++ qcrosvm.sh
@@ -3,9 +3,9 @@
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
-# We support maximum 16 bytes for audio variant,
-# and maximum 496 bytes for user-defined cmdline.
-# Hence, we define MAX_AUDIO_FW_LENGTH = 16 and MAX_USER_CMD_LENGTH = 496 in ABL.
+# We support maximum 4 bytes for audio variant,
+# and maximum 508 bytes for user-defined cmdline.
+# Hence, we define MAX_AUDIO_FW_LEN = 4 and MAX_USER_CMD_LENGTH = 508 in ABL.
 
 usage() {
     echo "usage: qcrosvm.sh -h: show help"
@@ -37,19 +37,25 @@ fi
 
 if [ "${audio_variant}" = "${AWE_VARIANT}" ]; then
   gvminfo="awe\0${user_cmdline}"
+  bias_count=2
 elif [ "${audio_variant}" = "${AR_VARIANT}" ]; then
   gvminfo="ar\0\0${user_cmdline}"
+  bias_count=4
 else
   gvminfo="\0\0\0\0${user_cmdline}"
+  bias_count=8
 fi
 
-seg1_count=$(($(echo ${gvminfo} | wc -c) - 2))
+seg1_count=$(($(echo ${gvminfo} | wc -c) - bias_count))
 seg2_count=$((${GVMINFO_IMG_SIZE} - ${seg1_count}))
 
 cd /tmp
+
 printf "${gvminfo}" | dd of=seg1.img bs=1 count=${seg1_count} 1>/dev/null 2>&1
 dd if=/dev/zero of=seg2.img bs=1 count=${seg2_count} 1>/dev/null 2>&1
 cat < seg2.img >> seg1.img
 mv seg1.img /tmp/${gvm_variant}_gvminfo.img
+
 rm -rf seg1.img seg2.img
+
 cd -
